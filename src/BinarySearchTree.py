65fd93361d461e71be9d95c7caa93d0bb17f78a2
Róger Roca Arano <rogerroca@reinseg.com>
2023-06-15 21:23:37 -0400

    Documentación.

--- src/BinarySearchTree.py
+++ src/BinarySearchTree.py
@@ -4,160 +4,39 @@ from src.Exceptions import *
 
 class BinarySearchTree:
     def __init__(self, tree_array: list = None):
+        """
+        Constructor for the BinarySearchTree class.
+        :param tree_array: Array of values to be inserted in the tree.
+        """
         self.__root = None
-        self.__deep = 0
         if tree_array is not None:
             for value in tree_array:
                 self.insert(value)
 
-    # ----------------------------------------------------------------------- #
-    # Getters and setters
+    """
+    A Binary Search Tree is a Binary Tree that has the following properties:
+    - The left subtree of a node contains only nodes with values lesser than 
+    the node's key.
+    - The right subtree of a node contains only nodes with keys greater than 
+    the node's key.
+    - The left and right subtree each must also be a binary search tree.
+    - It has a parameter called root that is the top node of the tree.
+    """
 
     @property
     def root(self):
         return self.__root
 
-    @property
-    def deep(self):
-        return self.__deep
-
     @root.setter
     def root(self, node: "BinaryTreeNode"):
         self.__root = node
 
-    # ----------------------------------------------------------------------- #
-    # Utility methods
-
-    def __str__(self, traversal_type: str = "inorder"):
-        """
-        Function for getting a string representation of the tree using the
-        specified traversal type.
-        :param traversal_type: Choose between inorder, preorder or postorder.
-        :return: A string representation of the tree.
-        """
-        tree_list = list()  # List for storing the tree nodes
-
-        # Pass the list to the traversal method to fill it
-        if traversal_type == "inorder":
-            self.in_order(self.root, tree_list)
-        elif traversal_type == "preorder":
-            self.pre_order(self.root, tree_list)
-        elif traversal_type == "postorder":
-            self.post_order(self.root, tree_list)
-        else:
-            return "Invalid traversal type."
-
-        # Return the list as a string
-        output = tree_list.__str__()
-        output = output + f"\nTree deep: {self.deep}"
-        return output
-
-    @staticmethod
-    def __is_leaf(node: "BinaryTreeNode"):
-        """
-        Private method for checking if a parent is a leaf.
-        :param node: Node to be checked.
-        :return: True if the parent is a leaf, False otherwise.
-        """
-        return node.left is None and node.right is None
-
-    @staticmethod
-    def __has_one_child(node: "BinaryTreeNode"):
-        """
-        Private method for checking if a parent has one child.
-        :param node: Node to be checked.
-        :return: True if the parent has one child, False otherwise.
-        """
-        return node.left is None or node.right is None
-
-    @staticmethod
-    def higher_from_left(root: "BinaryTreeNode"):
-        """
-        Private method for getting the highest parent in the left subtree.
-        :param root: Root of the subtree.
-        :return: Highest parent in the left subtree.
-        """
-        if root.left is None:
-            return root
-
-        higher = root.left
-        while higher.right is not None:
-            higher = higher.right
-        return higher
-
-    @staticmethod
-    def lower_from_right(root: "BinaryTreeNode"):
-        """
-        Private method for getting the lowest parent in the right subtree.
-        :param root: Root of the subtree.
-        :return: Lowest parent in the right subtree.
-        """
-        if root.right is None:
-            return root
-
-        lower = root.right
-        while lower.left is not None:
-            lower = lower.left
-        return lower
-
-    def __promote_higher_left(self, node):
-        """
-        Private method for promoting the highest parent in the left subtree.
-        :param node: Node to be promoted.
-        """
-        # Get needed nodes
-        higher = self.higher_from_left(node)
-        left = node.left
-        right = node.right
-
-        # Case parent is the root
-        if node is self.root:
-            self.root = higher
-
-        # Detach nodes
-        higher.make_orphan()
-        left.make_orphan()
-        right.make_orphan()
-        del node
-
-        higher.right = right
-        higher.left = left
-
-    def __promote_lower_right(self, node):
-        lower = self.lower_from_right(node)
-
-    # ----------------------------------------------------------------------- #
-    # Searching
-
-    def search(self, value):
-        """
-        Public method for searching a parent in the tree.
-        :param value: Value to be searched.
-        :return: True if the value is found, False otherwise.
-        """
-        return self.__search(self.root, value) is not None
-
-    def __search(self, root: "BinaryTreeNode", value):
-        """
-        Private method for searching a parent in the tree.
-        :param root: Starting parent for the search.
-        :param value: Value to be searched.
-        :return: Node with the data searched or None if not found.
-        """
-        # Case 1: Empty tree
-        if root.data is None:
-            return None
-        # Case 2: Value found
-        if root.data == value:
-            return root
-        # Case 3: recursive search
-        if value < root.data:
-            return self.__search(root.left, value)
-        else:
-            return self.__search(root.right, value)
-
-    # ----------------------------------------------------------------------- #
-    # Insertion
+    """
+    A Binary Search Tree has the following basic methods:
+    - insert: insert a value in the tree.
+    - search: search a value in the tree.
+    - delete: delete a value in the tree.
+    """
 
     def insert(self, value):
         """
@@ -193,8 +72,33 @@ class BinarySearchTree:
             else:
                 self.__insert(root.right, node)
 
-    # ----------------------------------------------------------------------- #
-    # Delete
+    def search(self, value):
+        """
+        Public method for searching a parent in the tree.
+        :param value: Value to be searched.
+        :return: True if the value is found, False otherwise.
+        """
+        return self.__search(self.root, value) is not None
+
+    def __search(self, root: "BinaryTreeNode", value):
+        """
+        Private method for searching a parent in the tree.
+        :param root: Starting parent for the search.
+        :param value: Value to be searched.
+        :return: Node with the data searched or None if not found.
+        """
+        # Case 1: Empty tree
+        if root.data is None:
+            return None
+        # Case 2: Value found
+        if root.data == value:
+            return root
+        # Case 3: recursive search
+        if value < root.data:
+            return self.__search(root.left, value)
+        else:
+            return self.__search(root.right, value)
+
     def delete(self, value, promote: str = "higher-left"):
         """
         Public method for deleting a parent from the tree.
@@ -220,6 +124,11 @@ class BinarySearchTree:
                 self.__promote_lower_right(node)
             else:
                 raise InvalidPromotionMethod
+
+    """
+    These methods are helper methods for the delete method.
+    In the deletion process, a parent can have 0, 1 or 2 children.
+    """
 
     def __delete_leaf_node(self, node: "BinaryTreeNode"):
         """
@@ -272,10 +181,92 @@ class BinarySearchTree:
         else:
             parent.right = child
 
-    # ----------------------------------------------------------------------- #
-    # Traversal
-    # A BST can be traversed through three basic algorithms: inorder, preorder,
-    # and postorder tree walks.
+    @staticmethod
+    def __is_leaf(node: "BinaryTreeNode"):
+        """
+        Private method for checking if a parent is a leaf.
+        :param node: Node to be checked.
+        :return: True if the parent is a leaf, False otherwise.
+        """
+        return node.left is None and node.right is None
+
+    @staticmethod
+    def __has_one_child(node: "BinaryTreeNode"):
+        """
+        Private method for checking if a parent has one child.
+        :param node: Node to be checked.
+        :return: True if the parent has one child, False otherwise.
+        """
+        return node.left is None or node.right is None
+
+    """
+    At deletion, a parent with two children can be promoted in two ways:
+    1. Promote the highest parent in the left subtree.
+    2. Promote the lowest parent in the right subtree.
+    """
+
+    def __promote_higher_left(self, node):
+        """
+        Private method for promoting the highest parent in the left subtree.
+        :param node: Node to be promoted.
+        """
+        # Get needed nodes
+        higher = self.higher_from_left(node)
+        left = node.left
+        right = node.right
+
+        # Case parent is the root
+        if node is self.root:
+            self.root = higher
+
+        # Detach nodes
+        higher.make_orphan()
+        left.make_orphan()
+        right.make_orphan()
+        del node
+
+        higher.right = right
+        higher.left = left
+
+    """
+    These methods are helper methods for the promotion process.
+    """
+
+    @staticmethod
+    def higher_from_left(root: "BinaryTreeNode"):
+        """
+        Private method for getting the highest parent in the left subtree.
+        :param root: Root of the subtree.
+        :return: Highest parent in the left subtree.
+        """
+        if root.left is None:
+            return root
+
+        higher = root.left
+        while higher.right is not None:
+            higher = higher.right
+        return higher
+
+    @staticmethod
+    def lower_from_right(root: "BinaryTreeNode"):
+        """
+        Private method for getting the lowest parent in the right subtree.
+        :param root: Root of the subtree.
+        :return: Lowest parent in the right subtree.
+        """
+        if root.right is None:
+            return root
+
+        lower = root.right
+        while lower.left is not None:
+            lower = lower.left
+        return lower
+
+    """
+    A BST can be traversed through three basic algorithms: inorder, preorder,
+    and postorder tree walks.
+    """
+
     def in_order(self, node: "BinaryTreeNode", tree: "list"):
         """
         Nodes from the left subtree get visited first, followed by the root
@@ -314,16 +305,15 @@ class BinarySearchTree:
             self.post_order(node.right, tree)
             tree.append(node.data)
 
-    # ----------------------------------------------------------------------- #
-    # Balance
-    # A BST is balanced if the height of the left and right subtrees of every
-    # parent differ by at most 1.
-    #
-    # Traverse given BST in inorder and store result in an array. Note that
-    # this array would be sorted as inorder.
-    # Build a balanced BST from the above created sorted array using a
-    # recursive approach. This step also takes O(n) time as we traverse every
-    # element exactly once and processing an element takes O(1) time.
+    """
+    A BST is balanced if the height of the left and right subtrees of every
+    parent differ by at most 1.
+    For balancing a BST, we can use the following approach:
+    - Traverse given BST in inorder and store result in an array. Note that
+    this array would be sorted as inorder.
+    - Build a balanced BST from the above created sorted array using a
+    recursive approach.
+    """
 
     def balance(self):
         """
@@ -354,7 +344,13 @@ class BinarySearchTree:
 
     @staticmethod
     def __get_balanced_tree(inorder_tree: list[BinaryTreeNode],
-                            balanced_tree: list[BinaryTreeNode]):
+                            balanced_tree: list[BinaryTreeNode]
+                            ):
+        """
+        Private method for getting a balanced tree.
+        :param inorder_tree: List of nodes in inorder.
+        :param balanced_tree: List to be filled with the balanced tree.
+        """
         # Base case:
         if len(inorder_tree) < 3:
             inorder_tree.reverse()
@@ -367,3 +363,33 @@ class BinarySearchTree:
         right_tree = inorder_tree[root_pos + 1:]
         BinarySearchTree.__get_balanced_tree(left_tree, balanced_tree)
         BinarySearchTree.__get_balanced_tree(right_tree, balanced_tree)
+
+    """
+    These methods are used for represent the node in a readable format.
+    """
+
+    def __repr__(self):
+        return f'BinarySearchTree({self.__root})'
+
+    def __str__(self, traversal_type: str = "inorder"):
+        """
+        Function for getting a string representation of the tree using the
+        specified traversal type.
+        :param traversal_type: Choose between inorder, preorder or postorder.
+        :return: A string representation of the tree.
+        """
+        tree_list = list()  # List for storing the tree nodes
+
+        # Pass the list to the traversal method to fill it
+        if traversal_type == "inorder":
+            self.in_order(self.root, tree_list)
+        elif traversal_type == "preorder":
+            self.pre_order(self.root, tree_list)
+        elif traversal_type == "postorder":
+            self.post_order(self.root, tree_list)
+        else:
+            return "Invalid traversal type."
+
+        # Return the list as a string
+        output = tree_list.__str__()
+        return output
--- src/BinaryTreeNode.py
+++ src/BinaryTreeNode.py
@@ -1,11 +1,12 @@
 class BinaryTreeNode:
     def __init__(self,
                  data,
-                 parent: "BinaryTreeNode" = None):
+                 parent: "BinaryTreeNode" = None
+                 ):
         """
         Constructor for the BinaryTreeNode class.
         :param data: data to be stored in the parent.
-        :param parent: parent parent.
+        :param parent: Parent of the current node.
         """
         self.__data = data
         self.__left: "BinaryTreeNode" = None
@@ -13,33 +14,69 @@ class BinaryTreeNode:
         self.__parent = parent
         self.__children = 0
 
-    # ----------------------------------------------------------------------- #
-    # Getter methods
+    """
+    A node in a Binary Tree formerly has this parameters:
+    - data: value to store on the node.
+    - left: the node on the left side.
+    - right: the node on the right side.
+    In addition for helping handling the data structure these parameters:
+    - parent: the node than the current node is child.
+    - children: the number of children than the node has.
+    """
 
     @property
     def data(self):
+        """
+        Getter method for data value.
+        :return: The value stored in the node.
+        """
         return self.__data
 
     @property
     def left(self):
+        """
+        Getter method for left node.
+        :return: The left child of the node.
+        """
         return self.__left
 
     @property
     def right(self):
+        """
+        Getter method for right node.
+        :return: The right child of the node.
+        """
         return self.__right
 
     @property
     def parent(self):
+        """
+        Getter method for getting the parent of the current node.
+        :return: The parent of the node.
+        """
         return self.__parent
 
     @property
     def children(self):
+        """
+        Getter method for getting the number of children a node has.
+        :return: Number of children of node.
+        """
         return self.__children
 
-    # ----------------------------------------------------------------------- #
-    # Setter methods
+    """
+    The parameters left, right and parent have setters for its 
+    respective values.
+    Te parameter children, is derived from the state of the left and 
+    right children.
+    """
+
     @left.setter
     def left(self, node: "BinaryTreeNode"):
+        """
+        Setter of the left child of the node.
+        :param node: Node to set as the left child.
+        """
         # If the value passed is None, reduce children by 1.
         if node is None:
             self.__left = None
@@ -58,6 +95,10 @@ class BinaryTreeNode:
 
     @right.setter
     def right(self, node: "BinaryTreeNode"):
+        """
+        Setter of the left child of the node.
+        :param node: Node to set as the right child.
+        """
         # If the value passed is None, reduce children by 1.
         if node is None:
             self.__right = None
@@ -76,6 +117,10 @@ class BinaryTreeNode:
 
     @parent.setter
     def parent(self, node: "BinaryTreeNode"):
+        """
+        Setter of the parent of the node.
+        :param node: Node to set as the parent.
+        """
         assert node.children < 2, \
             "Number of children must be less than max_children"
         # If parent is already a child of the parent, do nothing
@@ -88,11 +133,21 @@ class BinaryTreeNode:
         self.__parent = node
         node.__children += 1
 
-    # ----------------------------------------------------------------------- #
-    # Overloaded operators methods for comparing nodes
+    def make_orphan(self):
+        """
+        Method for making the node an orphan.
+        """
+        if self.parent.__left is self:
+            self.parent.__left = None
+        else:
+            self.parent.__right = None
+        self.parent.__children -= 1
+        self.__parent = None
 
-    def __repr__(self):
-        return f'Node({self.__data})'
+    """
+    These methods are overwrites for the default operators for comparing a 
+    node with other directly by the value stored on it.
+    """
 
     def __eq__(self, other):
         return self.__data == other.data
@@ -112,19 +167,12 @@ class BinaryTreeNode:
     def __ne__(self, other):
         return self.__data != other.data
 
-    # ----------------------------------------------------------------------- #
-    # Other methods
+    """
+    These methods are used for represent the node in a readable format.
+    """
 
-    def make_orphan(self):
-        """
-        Method for making the parent an orphan.
-        """
-        if self.parent.__left is self:
-            self.parent.__left = None
-        else:
-            self.parent.__right = None
-        self.parent.__children -= 1
-        self.__parent = None
+    def __repr__(self):
+        return f'Node({self.__data})'
 
     def __str__(self):
         return f'- Data: {self.data} ' \
